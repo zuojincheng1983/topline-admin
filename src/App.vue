7b5d9e2a294afbc2bb2cf3a2e083bdceb124fa28
zuojincheng1983 <lf_zuo@163.com>
2019-06-21 09:16:44 +0800

    修改代码格式,并引入了 element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <p>App 123123</p>
+    <H1>Vue + Element UI 实战案例</H1>
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+// 引入 element-ui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
+// 引入路由表
 import router from './router'
 
 Vue.use(ElementUI)
